7854fc665bc688c97f9dff735433583ed6244a98
Jisell Castro Villegas <1038407962@u.icesi.edu.co>
2025-10-24 12:57:20 -0500

    first

--- src/model/Incident.java
+++ src/model/Incident.java
@@ -1,5 +1,47 @@
 package model;
+import java.time.LocalDate;
 
-public class Incident {
-    
+public class Incident { 
+
+    private LocalDate Report; 
+    private String description; 
+    private boolean solution; 
+    private int solutionHours;
+
+    public incident (LocalDate Report, String description) {
+        this.Report = Report; 
+        this.description = description; 
+    }
+
+    public LocalDate getDateReport() {
+        return dateReport;
+    }
+
+    public String getDescription () {
+        return description;
+    }
+
+    public boolean getSolution () {
+        return solution;
+    }
+
+    public int getSolutionHours () {
+        return solutionHours;
+    }
+
+    public void setDateReport(LocalDate Report) {
+        this.Report = dateReport; 
+    }
+
+    public void setDescription(String description) {
+        this.Description = description; 
+    }                                  
+
+    public void setSolution(boolean solution){
+        this.Solution = solution;
+    }                            
+
+    public void setSolutionHours(int solutionHours) {
+        this.SolutionHours = solutionHours;
+    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
 }
